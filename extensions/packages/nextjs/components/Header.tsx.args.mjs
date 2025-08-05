50a00f668a07f670c868f20f475ba31424a4d634
metalboyrick <rsulisthio@gmail.com>
2025-08-05 12:39:13 +0700

    feat: add header templates implementation

---
+++ extensions/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,8 @@
+export const preContent = `import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";`;
+export const extraMenuLinksObjects = [
+  {
+    label: "ERC-20",
+    href: "/erc-20",
+    icon: '$$<MagnifyingGlassIcon className="h-4 w-4" />$$',
+  },
+];
