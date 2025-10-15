cdbe871735acfcea8f209d0b4a8e9520ce2febf1
tu11aa <thanhtuhr01@gmail.com>
2025-10-16 00:25:12 +0700

    feat: erc721 contract fix: change opt-level to 3 to reduce contract size

--- extensions/packages/stylus/erc721-example/src/lib.rs
+++ extensions/packages/stylus/erc721-example/src/lib.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
 extern crate alloc;
 
-use alloc::{string::String, vec::Vec};
+use alloc::string::String;
 
 use openzeppelin_stylus::{
     token::erc721::{
         self,
         extensions::{
-            Erc721Metadata, Erc721UriStorage, IErc721Burnable, IErc721Metadata, IErc721UriStorage,
+            enumerable, Erc721Enumerable, Erc721Metadata, IErc721Enumerable, IErc721Metadata,
         },
         Erc721, IErc721,
     },
@@ -17,7 +17,6 @@ use stylus_sdk::{
     abi::Bytes,
     alloy_primitives::{aliases::B32, Address, U256},
     prelude::*,
-    storage::StorageU256,
 };
 
 #[entrypoint]
@@ -25,36 +24,30 @@ use stylus_sdk::{
 struct Erc721MetadataExample {
     erc721: Erc721,
     metadata: Erc721Metadata,
-    uri_storage: Erc721UriStorage,
-    token_id_counter: StorageU256,
+    enumerable: Erc721Enumerable,
 }
 
 #[public]
-#[implements(IErc721<Error = erc721::Error>, IErc721Burnable<Error = erc721::Error>, IErc721Metadata<Error = erc721::Error>, IErc165)]
+#[implements(IErc721<Error = erc721::Error>, IErc721Metadata<Error = erc721::Error>, IErc721Enumerable<Error = enumerable::Error>, IErc165)]
 impl Erc721MetadataExample {
     #[constructor]
     fn constructor(&mut self, name: String, symbol: String, base_uri: String) {
         self.metadata.constructor(name, symbol);
         self.metadata.base_uri.set_str(base_uri);
-        self.token_id_counter.set(U256::from(0));
     }
 
     fn mint(&mut self, to: Address) -> Result<(), erc721::Error> {
-        self.token_id_counter
-            .set(self.token_id_counter.get() + U256::from(1));
-        let token_id = self.token_id_counter.get();
+        let token_id = self.enumerable.total_supply() + U256::from(1);
 
         match self.erc721._mint(to, token_id) {
             Ok(()) => {
-                let r = token_id % U256::from(3);
-                let uri = if r == U256::from(0) {
-                    "QmVHi3c4qkZcH3cJynzDXRm5n7dzc9R9TUtUcfnWQvhdcw"
-                } else if r == U256::from(1) {
-                    "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr"
-                } else {
-                    "QmcvcUaKf6JyCXhLD1by6hJXNruPQGs3kkLg2W1xr7nF1j"
-                };
-                self.uri_storage._set_token_uri(token_id, String::from(uri));
+                // update per-owner enumeration
+                let _ = self
+                    .enumerable
+                    ._add_token_to_owner_enumeration(to, token_id, &self.erc721);
+                // update global enumeration
+                self.enumerable
+                    ._add_token_to_all_tokens_enumeration(token_id);
                 Ok(())
             }
             Err(e) => Err(e),
@@ -62,12 +55,94 @@ impl Erc721MetadataExample {
     }
 
     fn total_supply(&self) -> U256 {
-        self.token_id_counter.get()
+        self.enumerable.total_supply()
     }
 
-    #[selector(name = "setTokenURI")]
-    fn set_token_uri(&mut self, token_id: U256, token_uri: String) {
-        self.uri_storage._set_token_uri(token_id, token_uri);
+    fn balance_of(&self, owner: Address) -> Result<U256, erc721::Error> {
+        self.erc721.balance_of(owner)
+    }
+
+    fn owner_of(&self, token_id: U256) -> Result<Address, erc721::Error> {
+        self.erc721.owner_of(token_id)
+    }
+
+    fn safe_transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), erc721::Error> {
+        self.erc721.safe_transfer_from(from, to, token_id)
+    }
+
+    fn safe_transfer_from_with_data(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+        data: Bytes,
+    ) -> Result<(), erc721::Error> {
+        self.erc721
+            .safe_transfer_from_with_data(from, to, token_id, data)
+    }
+
+    fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), erc721::Error> {
+        self.erc721.transfer_from(from, to, token_id)
+    }
+
+    fn approve(&mut self, to: Address, token_id: U256) -> Result<(), erc721::Error> {
+        self.erc721.approve(to, token_id)
+    }
+
+    fn set_approval_for_all(&mut self, to: Address, approved: bool) -> Result<(), erc721::Error> {
+        self.erc721.set_approval_for_all(to, approved)
+    }
+
+    fn get_approved(&self, token_id: U256) -> Result<Address, erc721::Error> {
+        self.erc721.get_approved(token_id)
+    }
+
+    fn is_approved_for_all(&self, owner: Address, operator: Address) -> bool {
+        self.erc721.is_approved_for_all(owner, operator)
+    }
+
+    fn name(&self) -> String {
+        self.metadata.name()
+    }
+
+    fn symbol(&self) -> String {
+        self.metadata.symbol()
+    }
+
+    #[selector(name = "tokenURI")]
+    fn token_uri(&self, token_id: U256) -> Result<String, erc721::Error> {
+        // Compute URI based on token_id % 3 without storing per-token strings
+        let r = token_id % U256::from(3);
+        let cid = if r == U256::from(0) {
+            "QmVHi3c4qkZcH3cJynzDXRm5n7dzc9R9TUtUcfnWQvhdcw"
+        } else if r == U256::from(1) {
+            "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr"
+        } else {
+            "QmcvcUaKf6JyCXhLD1by6hJXNruPQGs3kkLg2W1xr7nF1j"
+        };
+        Ok(self.metadata.base_uri() + cid)
+    }
+
+    fn token_of_owner_by_index(
+        &self,
+        owner: Address,
+        index: U256,
+    ) -> Result<U256, enumerable::Error> {
+        Ok(self.enumerable.token_of_owner_by_index(owner, index)?)
+    }
+
+    fn token_by_index(&self, index: U256) -> Result<U256, enumerable::Error> {
+        Ok(self.enumerable.token_by_index(index)?)
     }
 }
 
@@ -109,7 +184,18 @@ impl IErc721 for Erc721MetadataExample {
         to: Address,
         token_id: U256,
     ) -> Result<(), Self::Error> {
-        self.erc721.transfer_from(from, to, token_id)
+        let previous_owner = self.erc721.owner_of(token_id)?;
+        self.erc721.transfer_from(from, to, token_id)?;
+        // update enumerations
+        let _ = self.enumerable._remove_token_from_owner_enumeration(
+            previous_owner,
+            token_id,
+            &self.erc721,
+        );
+        let _ = self
+            .enumerable
+            ._add_token_to_owner_enumeration(to, token_id, &self.erc721);
+        Ok(())
     }
 
     fn approve(&mut self, to: Address, token_id: U256) -> Result<(), Self::Error> {
@@ -130,15 +216,6 @@ impl IErc721 for Erc721MetadataExample {
 }
 
 #[public]
-impl IErc721Burnable for Erc721MetadataExample {
-    type Error = erc721::Error;
-
-    fn burn(&mut self, token_id: U256) -> Result<(), Self::Error> {
-        self.erc721._burn(token_id)
-    }
-}
-
-#[public]
 impl IErc721Metadata for Erc721MetadataExample {
     type Error = erc721::Error;
 
@@ -152,17 +229,32 @@ impl IErc721Metadata for Erc721MetadataExample {
 
     #[selector(name = "tokenURI")]
     fn token_uri(&self, token_id: U256) -> Result<String, Self::Error> {
-        self.uri_storage
-            .token_uri(token_id, &self.erc721, &self.metadata)
+        Ok(self.metadata.token_uri(token_id, &self.erc721)?)
     }
 }
 
-impl IErc721UriStorage for Erc721MetadataExample {}
+#[public]
+impl IErc721Enumerable for Erc721MetadataExample {
+    type Error = enumerable::Error;
+
+    fn total_supply(&self) -> U256 {
+        self.enumerable.total_supply()
+    }
+
+    fn token_by_index(&self, index: U256) -> Result<U256, Self::Error> {
+        Ok(self.enumerable.token_by_index(index)?)
+    }
+
+    fn token_of_owner_by_index(&self, owner: Address, index: U256) -> Result<U256, Self::Error> {
+        Ok(self.enumerable.token_of_owner_by_index(owner, index)?)
+    }
+}
 
 #[public]
 impl IErc165 for Erc721MetadataExample {
     fn supports_interface(&self, interface_id: B32) -> bool {
         self.erc721.supports_interface(interface_id)
             || <Self as IErc721Metadata>::interface_id() == interface_id
+            || <Self as IErc721Enumerable>::interface_id() == interface_id
     }
 }
